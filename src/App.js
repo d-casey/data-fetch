a4436cf79d6cc308cae952700c121916177e964c
Damian Casey <damian.casey@HX3347T2.local>
2020-07-04 10:25:10 +0100

    even simpler when you chain the async calls when the component mounts...

--- src/App.js
+++ src/App.js
@@ -5,29 +5,18 @@ import './App.css'
 
 const App = () => {
   const [pokemon, setPokemon] = useState(null)
-  const [formatted, setFormatted] = useState(false)
 
   useEffect(() => {
     try {
       fetchPokemon()
-        .then(data => setPokemon(data))
+        .then(data => {
+          formatResults(data)
+            .then(formattedData => setPokemon(formattedData))
+        })
     } catch (err) {
       console.log('error fetching initial pokemon data', err)
     }
-
   }, [])
-
-  useEffect(() => {
-    if (!formatted && pokemon) {
-      try {
-        formatResults(pokemon)
-          .then(data => setPokemon(data))
-          .then(setFormatted(true))
-      } catch(err) {
-        console.log('error formatting received pokemon data', err)
-      }
-    }
-  }, [pokemon])
 
   return (
     <div className="App">
@@ -35,7 +24,7 @@ const App = () => {
         <h1>Give me Pokemon!</h1>
       </header>
       <main className="Main">
-        { formatted && pokemon &&
+        { pokemon &&
           <PokemonList pokemon={pokemon} />
         }
       </main>
